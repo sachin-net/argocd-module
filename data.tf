95ceb1dd02ce2b7fddd8ac8e78b09a1eb44dfb85
EC2 Default User <ec2-user@ip-172-31-29-141.eu-west-2.compute.internal>
2024-07-05 12:48:29 +0000

    third commit

--- data.tf
+++ data.tf
@@ -10,3 +10,13 @@ data "aws_eks_cluster_auth" "cluster" {
 data "external" "guestbook_status" {
   program = ["${path.module}/bin/get_app_status.sh"]
 }
+
+# Fetch the resource details of the guestbook application
+# data "kubernetes_resource" "harness_gitops_agent" {
+#   api_version = "argoproj.io/v1alpha1"
+#   kind        = "Application"
+#   metadata {
+#     name      = "guestbook"
+#     namespace = "argocd"  # Adjust the namespace if needed
+#   }
+# }
